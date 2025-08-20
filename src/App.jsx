8d9c1c0c55954eef054155ade8e5de9f6bd8d7c5
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-08-21 00:29:51 +0300

    Imported useState hook in App.jsx to create the bridge between PersonalInfo and CVPreview

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import PersonalInfoForm from "./components/PersonalInfoForm";
+import CVPreview from "./components/CVPreview";
 import "./App.css";
 
 function App() {
+  const [personalInfo, setPersonalInfo] = useState({
+    firstName: "Test FirstName",
+    lastName: "Test LastName",
+    email: "testing@office.com",
+    phoneNumber: "+44 768 521 55",
+  });
+
+  const [submittedInfo, setSubmittedInfo] = useState(personalInfo);
+
+  const handleSubmit = () => {
+    setSubmittedInfo(personalInfo);
+  };
+
   return (
     <>
       <Header />
-      <PersonalInfoForm />
+      <PersonalInfoForm
+        personalInfo={personalInfo}
+        setPersonalInfo={setPersonalInfo}
+        onSubmit={handleSubmit}
+      />
+      <CVPreview personalInfo={submittedInfo} />
     </>
   );
 }
