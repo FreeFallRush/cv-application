ae65377a0e1a409970af88453c229d2f42923c9a
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-08-20 23:03:29 +0300

    Added PersonalInfoForm.jsx in components with PersonalInfoForm component
    Imported PersonalInfoForm in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
-
+import PersonalInfoForm from "./components/PersonalInfoForm";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Header />
+      <PersonalInfoForm />
     </>
   );
 }
---
+++ src/components/PersonalInfoForm.jsx
@@ -0,0 +1,12 @@
+function PersonalInfoForm() {
+  return (
+    <>
+      <h2>Add Personal Info</h2>
+      <form>
+        <input name="firstName" type="text" placeholder="First Name" />
+      </form>
+    </>
+  );
+}
+
+export default PersonalInfoForm;
