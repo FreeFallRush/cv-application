a7a0fe9efac2494b87c18a56a6c311a879eab7bd
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-08-20 23:45:53 +0300

    Import useState in PersonalInfoForm and tested input for firstName

--- src/components/PersonalInfoForm.jsx
+++ src/components/PersonalInfoForm.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 function PersonalInfoForm() {
+  const [firstName, setFirstName] = useState("");
+
+  function handleFirstName(e) {
+    setFirstName(e.target.value);
+  }
   return (
     <>
       <h2>Add Personal Info</h2>
       <form>
-        <input name="firstName" type="text" placeholder="First Name" />
+        <input
+          name="firstName"
+          type="text"
+          placeholder="First Name"
+          value={firstName}
+          onChange={handleFirstName}
+        />
       </form>
+      <p>First Name: {firstName}</p>
     </>
   );
 }
