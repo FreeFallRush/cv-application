4912fd5bb6a9ff4906921aa354edfd8df5a50179
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-08-21 00:33:23 +0300

    Added functionality to display data in CVPreviewer.jsx

--- src/components/CVPreview.jsx
+++ src/components/CVPreview.jsx
@@ -1,3 +1,24 @@
-function CVPreview() {}
+function CVPreview(personalInfo) {
+  return (
+    <>
+      <p>
+        <strong>First Name:</strong>
+        {personalInfo.firstName}
+      </p>
+      <p>
+        <strong>Last Name:</strong>
+        {personalInfo.lastName}
+      </p>
+      <p>
+        <strong>E-mail:</strong>
+        {personalInfo.email}
+      </p>
+      <p>
+        <strong>Phone Number:</strong>
+        {personalInfo.phoneNumber}
+      </p>
+    </>
+  );
+}
 
 export default CVPreview;
