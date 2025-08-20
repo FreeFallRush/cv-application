8e9b4653d39f5d5df9daed1247ee499aec8fdf59
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-08-21 00:10:54 +0300

    Added more info inputs in PersonalInfoForm.jsx

--- src/components/PersonalInfoForm.jsx
+++ src/components/PersonalInfoForm.jsx
@@ -2,23 +2,70 @@ import { useState } from "react";
 
 function PersonalInfoForm() {
   const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
 
   function handleFirstName(e) {
     setFirstName(e.target.value);
   }
+
+  function handleLastName(e) {
+    setLastName(e.target.value);
+  }
+
+  function handleEmail(e) {
+    setEmail(e.target.value);
+  }
+
+  function handlePhoneNumber(e) {
+    setPhoneNumber(e.target.value);
+  }
+
   return (
     <>
       <h2>Add Personal Info</h2>
       <form>
         <input
           name="firstName"
+          maxLength={60}
           type="text"
           placeholder="First Name"
           value={firstName}
           onChange={handleFirstName}
         />
+
+        <input
+          name="lastName"
+          type="text"
+          maxLength={60}
+          placeholder="Last Name"
+          value={lastName}
+          onChange={handleLastName}
+        />
+
+        <input
+          name="email"
+          maxLength={60}
+          type="text"
+          placeholder="E-mail"
+          value={email}
+          onChange={handleEmail}
+        />
+
+        <input
+          name="phoneNumber"
+          maxLength={60}
+          type="text"
+          placeholder="Phone Number"
+          value={phoneNumber}
+          onChange={handlePhoneNumber}
+        />
       </form>
       <p>First Name: {firstName}</p>
+      <p>Last Name: {lastName}</p>
+      <p>E-mail: {email}</p>
+      <p>Phone Number: {phoneNumber}</p>
     </>
   );
 }
