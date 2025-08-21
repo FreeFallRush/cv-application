406606807477fcebe9641e14d429b9cd17a7299b
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-08-21 13:47:48 +0300

    Added CVPreview.css with styles for cv preview
    Imported stylesin CVPreview.jsx

--- src/components/CVPreview.jsx
+++ src/components/CVPreview.jsx
@@ -1,31 +1,36 @@
+import "../styles/CVPreview.css";
+
 function CVPreview({ personalInfo, defaultInfo }) {
   return (
-    <>
-      <p>
-        <strong>First Name: </strong>
-        {personalInfo.firstName || defaultInfo.firstName}
-      </p>
-      <p>
-        <strong>Last Name: </strong>
-        {personalInfo.lastName || defaultInfo.lastName}
-      </p>
-      <p>
-        <strong>E-mail: </strong>
-        {personalInfo.email || defaultInfo.email}
-      </p>
-      <p>
-        <strong>Phone Number: </strong>
-        {personalInfo.phoneNumber || defaultInfo.phoneNumber}
-      </p>
-      <p>
-        <strong>City: </strong>
-        {personalInfo.city || defaultInfo.city}
-      </p>
-      <p>
-        <strong>Country: </strong>
-        {personalInfo.country || defaultInfo.country}
-      </p>
-    </>
+    <div className="cv-preview">
+      <h2>Curriculum Vitae</h2>
+      <div className="personalInfo">
+        <p>
+          <strong>First Name: </strong>
+          {personalInfo.firstName || defaultInfo.firstName}
+        </p>
+        <p>
+          <strong>Last Name: </strong>
+          {personalInfo.lastName || defaultInfo.lastName}
+        </p>
+        <p>
+          <strong>E-mail: </strong>
+          {personalInfo.email || defaultInfo.email}
+        </p>
+        <p>
+          <strong>Phone Number: </strong>
+          {personalInfo.phoneNumber || defaultInfo.phoneNumber}
+        </p>
+        <p>
+          <strong>City: </strong>
+          {personalInfo.city || defaultInfo.city}
+        </p>
+        <p>
+          <strong>Country: </strong>
+          {personalInfo.country || defaultInfo.country}
+        </p>
+      </div>
+    </div>
   );
 }
 
