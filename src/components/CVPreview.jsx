c88973b79bcadaed0ae032141d376564585184d8
FreeFallRush <84788709+FreeFallRush@users.noreply.github.com>
2025-08-21 13:54:10 +0300

    Added more structure to CVPreview.jsx
    Added styles to CVPreview.css

--- src/components/CVPreview.jsx
+++ src/components/CVPreview.jsx
@@ -4,31 +4,34 @@ function CVPreview({ personalInfo, defaultInfo }) {
   return (
     <div className="cv-preview">
       <h2>Curriculum Vitae</h2>
-      <div className="personalInfo">
-        <p>
-          <strong>First Name: </strong>
-          {personalInfo.firstName || defaultInfo.firstName}
-        </p>
-        <p>
-          <strong>Last Name: </strong>
-          {personalInfo.lastName || defaultInfo.lastName}
-        </p>
-        <p>
-          <strong>E-mail: </strong>
-          {personalInfo.email || defaultInfo.email}
-        </p>
-        <p>
-          <strong>Phone Number: </strong>
-          {personalInfo.phoneNumber || defaultInfo.phoneNumber}
-        </p>
-        <p>
-          <strong>City: </strong>
-          {personalInfo.city || defaultInfo.city}
-        </p>
-        <p>
-          <strong>Country: </strong>
-          {personalInfo.country || defaultInfo.country}
-        </p>
+      <div className="personalInfo-section">
+        <h3>Personal Info</h3>
+        <div className="personalInfo">
+          <p>
+            <strong>First Name: </strong>
+            {personalInfo.firstName || defaultInfo.firstName}
+          </p>
+          <p>
+            <strong>Last Name: </strong>
+            {personalInfo.lastName || defaultInfo.lastName}
+          </p>
+          <p>
+            <strong>E-mail: </strong>
+            {personalInfo.email || defaultInfo.email}
+          </p>
+          <p>
+            <strong>Phone Number: </strong>
+            {personalInfo.phoneNumber || defaultInfo.phoneNumber}
+          </p>
+          <p>
+            <strong>City: </strong>
+            {personalInfo.city || defaultInfo.city}
+          </p>
+          <p>
+            <strong>Country: </strong>
+            {personalInfo.country || defaultInfo.country}
+          </p>
+        </div>
       </div>
     </div>
   );
